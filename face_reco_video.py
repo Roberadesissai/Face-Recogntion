9fa40f6760e5656d2b893ecd1ef5f5235ecf4312
Roberadesissai <roberadesissakit@gmail.com>
2022-09-07 00:25:13 -0400

    add face recognition

--- face_reco_video.py
+++ face_reco_video.py
@@ -1,18 +1,28 @@
 import cv2
-import face_recognition
 from simple_facerec import SimpleFacerec
 
-
 sfr = SimpleFacerec()
-sfr.load_encoding_images("image/")
+sfr.load_encoding_images("images/")
 
-video = cv2.VideoCapture(0)
+
+cap = cv2.VideoCapture(0)
+
 
 while True:
-    ret, frame = video.read()
+    ret, frame = cap.read()
 
-    cv2.imshow("Video", frame)
+    face_locations, face_names = sfr.detect_known_faces(frame)
+    for face_loc, name in zip(face_locations, face_names):
+        y1, x2, y2, x1 = face_loc[0], face_loc[1], face_loc[2], face_loc[3]
+
+        cv2.putText(frame, name,(x1, y1 - 10), cv2.FONT_HERSHEY_DUPLEX, 1, (255, 255, 255), 2)
+        cv2.rectangle(frame, (x1, y1), (x2, y2), (255, 255, 255), 3)
+
+    cv2.imshow("Frame", frame)
 
     key = cv2.waitKey(1)
     if key == ord('q'):
         break
+
+cap.release()
+cv2.destroyAllWindows()
